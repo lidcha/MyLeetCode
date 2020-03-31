d896db884414e33c60ad7baf856891596bb65d18
lidcha <2364633786@qq.com>
2020-03-31 12:01:17 +0800

    commit

--- src/reverseBits_190/ReverseBits_190.java
+++ src/reverseBits_190/ReverseBits_190.java
@@ -7,23 +7,26 @@ import org.junit.Test;
 import com.sun.org.apache.bcel.internal.generic.SWAP;
 
 /**
-* @author 
-* @createDate ：2020年3月30日
-*/
+ * @author
+ * @createDate ：2020年3月30日
+ */
 public class ReverseBits_190 {
 	// you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        String string = Integer.toUnsignedString(n);
-        char[] charArray = string.toCharArray();
-        for(int i=0;i<charArray.length;i++){
-        	char c = charArray[i];
-        	charArray[i] = charArray[charArray.length-1-i];
-        	charArray[charArray.length-1-i]=c;
-        }
-        return Integer.parseUnsignedInt(string);
-    }
-    @Test
-    public void test(){
-    	assertEquals(964176192, new ReverseBits_190().reverseBits(43261596));
-    }
+	/*
+	 * 题解：利用位运算，共32位，从个位开始，不断将res左移，并且加上n的个位，再将n右移一位，有了新的个位
+	 * 其中n&1=1代表n的个位是1，n&1=0代表n的个位是0；
+	 */
+	public int reverseBits(int n) {
+		int res = 0;
+		for(int i=0;i<32;i++){
+			res = (res<<1)+(n&1);
+			n=n>>1;
+		}
+		return res;
+	}
+
+	@Test
+	public void test() {
+		assertEquals(964176192, new ReverseBits_190().reverseBits(43261596));
+	}
 }
