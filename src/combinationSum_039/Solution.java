d8d3b7a0fc394166f2a007cb0b3252caf2922412
lidcha <2364633786@qq.com>
2020-04-12 20:53:19 +0800

    commit

---
+++ src/combinationSum_039/Solution.java
@@ -0,0 +1,44 @@
+package combinationSum_039;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.org.apache.xml.internal.security.utils.HelperNodeList;
+
+/**
+ * @description
+ * @createDate ：2020年4月12日
+ */
+public class Solution {
+	
+	/*
+	 * 所谓回溯法，需要多看看
+	 */
+	List<List<Integer>> res = new ArrayList<>();
+
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<Integer> list = new ArrayList<>();
+		Arrays.sort(candidates);
+		helper(candidates, list, target, 0, 0);
+		return res;
+	}
+
+	public void helper(int[] candiates, List<Integer> list, int target, int sum, int index) {
+		if (sum > target)
+			return;
+		if (sum == target) {
+			res.add(new ArrayList<>(list));
+			return;
+		}
+		for (int i = 0; i < candiates.length; i++) {
+			if (sum + candiates[i] <= target) {
+				list.add(candiates[i]);
+				helper(candiates, list, target, sum, i);
+				list.remove(list.size()-1);
+			} else
+				break;
+		}
+	}
+}
