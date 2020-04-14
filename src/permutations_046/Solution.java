2f5209b4b7d6e5b7e2700595c816651533da851d
lidcha <2364633786@qq.com>
2020-04-14 13:54:34 +0800

    commit

---
+++ src/permutations_046/Solution.java
@@ -0,0 +1,47 @@
+package permutations_046;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.javafx.image.IntPixelAccessor;
+import com.sun.org.apache.xml.internal.security.utils.HelperNodeList;
+
+/**
+ * @description Given a collection of distinct integers, return all possible
+ *              permutations.
+ * 
+ *              Example:
+ * 
+ *              Input: [1,2,3] Output: [ [1,2,3], [1,3,2], [2,1,3], [2,3,1],
+ *              [3,1,2], [3,2,1] ]
+ * @createDate ：2020年4月13日
+ */
+public class Solution {
+	List<List<Integer>> res = new ArrayList<>(); // 用于结果返回
+
+	public List<List<Integer>> permute(int[] nums) {
+		Arrays.sort(nums);
+		List<Integer> list = new ArrayList<>();
+		helper(nums, list);
+		return res;
+	}
+
+	// 用于回溯的方法
+	public void helper(int[] nums, List<Integer> list) {
+		if (list.size() > nums.length)
+			return;
+		if (list.size() == nums.length) {
+			res.add(new ArrayList<>(list));
+			return;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (list.contains(nums[i]))
+				continue;
+			list.add(nums[i]);
+			helper(nums, list);
+			list.remove(list.size() - 1);
+		}
+
+	}
+}
