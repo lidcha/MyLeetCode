4f6d38cc654e91a1ad6f0a70760ff722eecbb252
lidcha <2364633786@qq.com>
2020-04-15 21:15:02 +0800

    commmit

--- src/permutations_046/Solution.java
+++ src/permutations_046/Solution.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import com.sun.javafx.image.IntPixelAccessor;
 import com.sun.org.apache.xml.internal.security.utils.HelperNodeList;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @description Given a collection of distinct integers, return all possible
@@ -43,5 +46,17 @@ public class Solution {
 			list.remove(list.size() - 1);
 		}
 
+	}
+	@Test
+	public void test() {
+		int[] nums = new int[] { 1, 2, 3 };
+		List<List<Integer>> expected = new ArrayList<>();
+		expected.add(Arrays.asList(1, 2, 3));
+		expected.add(Arrays.asList(1, 3, 2));
+		expected.add(Arrays.asList(2, 1, 3));
+		expected.add(Arrays.asList(2, 3, 1));
+		expected.add(Arrays.asList(3, 1, 2));
+		expected.add(Arrays.asList(3, 2, 1));
+		assertEquals(expected, new permutations_046.Solution().permute(nums));
 	}
 }
--- src/permutations_047/Solution.java
+++ src/permutations_047/Solution.java
@@ -25,19 +25,26 @@ public class Solution {
 
 	public List<List<Integer>> permuteUnique(int[] nums) {
 		List<Integer> list = new ArrayList<>();
-		helper(nums, list);
+		int[] visited = new int[nums.length];
+		Arrays.sort(nums);
+		for(int i=0;i<nums.length;i++)
+			visited[i]=0;
+		helper(nums, list,visited);
 		return res;
 	}
 
-	public void helper(int[] nums, List<Integer> list) {
+	public void helper(int[] nums, List<Integer> list, int[] visited) {
 		if (list.size() == nums.length) {
 			res.add(new ArrayList<>(list));
 			return;
 		}
 		for (int i = 0; i < nums.length; i++) {
-			if(list.contains(nums[i])) continue;
+			if(visited[i]==1) continue;
+			if(i>0&&nums[i]==nums[i-1]&&visited[i-1]==1)	continue;
 			list.add(nums[i]);
-			helper(nums, list);
+			visited[i]=1;
+			helper(nums, list, visited);
+			visited[i]=0;
 			list.remove(list.size() - 1);
 		}
 	}
